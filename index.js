cbb148597884a4c4c130703bc2a134060df74fdc
reaganongaya <reagan.ongaya@student.moringaschool.com>
2022-11-30 13:05:11 +0300

    Assignment Completed

---
+++ index.js
@@ -0,0 +1,20 @@
+function myMonday(){
+    console.log('It starts with a morning run.')
+}
+
+myMonday()
+
+
+function myLogo(name){
+    console.log("Greetings");
+    return "hello";
+}
+
+myLogo();
+
+function returnsAnAnonymousFunction(){
+    console.log('bye')
+    return "bye";
+}
+
+returnsAnAnonymousFunction()
